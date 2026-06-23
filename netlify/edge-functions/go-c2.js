83d652a5ec3537ac2f6e8a167ad252873e50e44b
lucasinavo-netizen <lucasinavo@gmail.com>
2026-06-23 13:11:04 +0800

    SEO: use Netlify redirects for affiliate routes

--- netlify/edge-functions/go-c2.js
+++
@@ -1,6 +0,0 @@
-// Affiliate redirect: MM38AA branch.
-export default async () => Response.redirect('https://www.mm38aa.com/m/home?affiliateCode=lu0001', 302);
-
-export const config = {
-  path: '/go/c2',
-};
